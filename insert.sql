04259bdf8408def1bd4c1b4e259aa250989cbed8
Daniele Barroso <danielesilvabarroso@gmail.com>
2026-04-27 22:12:47 -0300

    feat: adição de Insert no banco de dados

---
+++ insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO Produto(nome, descricao, preco, categoria, disponivel)
+VALUES (
+"Expresso Brasileiro Intenso", "Forte, encorpado, com gotas de chocolate amargo e nozes", 8.5, "Cafés", true),
+("Capuccino de Doce de Leite", "Expresso cremoso com calda de doce de leite e canela.", 12, "Cafés", true),
+("Mocha de Castanha-do-Pará", "Expresso, chocolate e leite de castanha-do-pará. Finalizado com chantilly e raspas de castanha.", 10, "Cafés", true),
+("Latte de Paçoca", "Expresso com creme de paçoca e paçoca triturada por cima. Doce, crocante e brasileiro.", 10, "Cafés", true),
+("Café com Leite", "Expresso com leite e gotas de baunilha, adocicando o amargo do café.", 10, "Cafés", true),
+("Cold Brew Natural", "Café extraído a frio. Suave, sem amargor e refrescante.", 10, "Cafés", true),
+("Café Brigadeiro", "Chocolate com leite condensado, doce e viciante, permanecendo o armagor do café com suavidade.", 10, "Cafés", true),
+("Café Caramelizado", "Café, leite e caramelo são colocados juntos, tendo um doce sabor.", 10, "Cafés", true);
