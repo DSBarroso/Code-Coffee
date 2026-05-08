b171158a29255ba3ed6ec032ec41b820cd677e75
Daniele Barroso <danielesilvabarroso@gmail.com>
2026-05-08 09:54:14 -0300

    feat: Insert data into Mesa and cliente tables

--- insert.sql
+++ insert.sql
@@ -10,3 +10,21 @@ VALUES (
 ("Cold Brew Natural", "Café extraído a frio. Suave, sem amargor e refrescante.", 10, "Cafés", true),
 ("Café Brigadeiro", "Chocolate com leite condensado, doce e viciante, permanecendo o armagor do café com suavidade.", 10, "Cafés", true),
 ("Café Caramelizado", "Café, leite e caramelo são colocados juntos, tendo um doce sabor.", 10, "Cafés", true);
+
+INSERT INTO Mesa(numero, capacidade)
+VALUES (
+1, 4),
+(2, 6),
+(3, 4),
+(4, 6),
+(5, 4),
+(6, 6),
+(7, 4),
+(8, 6);
+
+INSERT INTO cliente (nome, email, senha, telefone)
+VALUES ("João", "joaomenezes@gmail.com", "gutajota789@", "11 96785-3056"),
+("Maria José", "mariajosesilva@hotmail.com", "mjsilva07891", "11 95632-4599"),
+("Carmen Souza", "souzacarmen@gmail.com", "csmoreira456723", "11 93756-4872"),
+("Carlos Montes", "cmontes56@gmail.com", "montescarlos@5623", "11 98627-2257"),
+("José Barbosa", "josebarbosa@hotmail.com", "873barb#", "11 93266-5816");
